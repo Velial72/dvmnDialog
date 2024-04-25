5c89f3750de5453254cb1dfb21319538ef07be71
Velial72 <126498671+Velial72@users.noreply.github.com>
2024-04-25 21:53:43 +0500

    setup DialogFlow

--- main.py
+++ main.py
@@ -3,24 +3,23 @@ from aiogram.filters import Command
 from aiogram.types import Message
 from environs import Env
 
+from dialog import detect_intent_text
 
 env = Env()
 env.read_env()
 
 BOT_TOKEN = env('BOT_TOKEN')
 
-# Создаем объекты бота и диспетчера
+
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
-# Этот хэндлер будет срабатывать на команду "/start"
 @dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
-    await message.answer('Привет!\nНапиши мне что-нибудь')
+    await message.answer('Zzz...')
 
 
-# Этот хэндлер будет срабатывать на команду "/help"
 @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(
@@ -29,11 +28,10 @@ async def process_help_command(message: Message):
     )
 
 
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
-# кроме команд "/start" и "/help"
 @dp.message()
-async def send_echo(message: Message):
-    await message.reply(text=message.text)
+async def send_answer(message: Message):
+    serialized_answer = detect_intent_text('dvmn-bot-ynkq', message.from_user.id, message.text, 'ru')
+    await bot.send_message(chat_id=message.chat.id, text=serialized_answer['answer'])
 
 
 if __name__ == '__main__':
